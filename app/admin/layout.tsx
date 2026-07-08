6d805019729052f20b653d8c2a601b7ae566f588
MOUNJEDDINE Ibrahim <imounjeddine@gmail.com>
2026-07-09 00:37:04 +0100

    phase11: noindex admin control center

---
+++ app/admin/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'TRH Control Center',
+  robots: { index: false, follow: false },
+}
+
+export default function AdminLayout({ children }: { children: React.ReactNode }) {
+  return children
+}
