236ac1ebe473d2ad5df00e063f3460f179dc1769
MOUNJEDDINE Ibrahim <imounjeddine@gmail.com>
2026-07-09 00:04:55 +0100

    phase10: noindex private staff access route

---
+++ app/trh-staff/access/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: 'TRH staff access',
+  robots: { index: false, follow: false },
+}
+
+export default function StaffAccessLayout({ children }: { children: React.ReactNode }) {
+  return children
+}
